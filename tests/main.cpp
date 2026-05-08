8057f011c6b0e375afad4d908e12a78bdd230bd8
evgeniy.gleba <i@egleba.ru>
2026-05-09 00:05:42 +0300

    disable print as not supproted for all apis

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,24 +7,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
 
-int main(int argc, char* argv[], char* envp[])
+int main(int argc, char* argv[], [[maybe_unused]] char* envp[])
 {
-    std::println("-- doctest runner --");
-    std::println("argc: {}", argc);
-
-    std::println("argv:");
-    for (std::size_t i = 0;
-         auto*       s : std::span{ argv, static_cast<std::size_t>(argc) }) {
-        std::println("  [{:>2}] {}", i++, s);
-    }
-
-    std::println("envp (first 10 non-null):");
-    for (std::size_t i = 0;
-         auto*       e :
-         std::span{ envp, 10 } | std::views::take_while(std::identity{})) {
-        std::println("  [{:>2}] {}", i++, e);
-    }
-
     doctest::Context ctx{ argc, argv };
     ctx.setOption("duration", true);
 
